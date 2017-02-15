009c5c9e0c93d9205732e734e9a8cfdce80360dd
Boyan Kushlev <takarabukara@gmail.com>
2017-02-16 00:33:46 +0200

    Update speech_util.py

--- speech_util.py
+++ speech_util.py
@@ -8,13 +8,17 @@ class SpeechUtil(object):
     def __init__(self):
         self.player = pyglet.app
         
-    ''' using the Google Text-to-Speech API the string passed_text is processed by the same software 
-        used in Google Translate, and is then saved into an mp3 file for later playback. '''
+    ''' 
+    using the Google Text-to-Speech API the string passed_text is processed by the same software 
+    used in Google Translate, and is then saved into an mp3 file for later playback. 
+    '''
     def read_the_given_text(self, passed_text):
         text = gTTS(text=passed_text, lang='en')
         text.save(os.getcwd() + os.sep + 'text.mp3')
 
-    ''' the text is firstly processed and saved as an mp3 and then played using the pyglet player '''
+    ''' 
+    the text is firstly processed and saved as an mp3 and then played using the pyglet player 
+    '''
     def say(self, passed_text):
         self.read_the_given_text(passed_text)
         f = pyglet.media.load(os.getcwd() + os.sep + 'text.mp3', streaming=False)
